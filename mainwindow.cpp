d6dcde555090ddb0dc90698a9d424e002e87c7ad
ChengCarlos <ChengCarlos@users.noreply.github.com>
2018-07-30 16:02:41 +0800

    微调icon位置

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -842,6 +842,7 @@ void MainWindow::on_pushButton_9_clicked()
                file_name = query.record().value("file_name").toString();
                file_size = query.record().value("article_size").toString();
 
+
                file_id = query.record().value("file_id").toString();
 
                file_discryption = "文件已成功解密。";
